3d92082ddf1dedda01844d70d7d434739a06d6dc
Michael Rodriguez <33561579+MichaelRL1@users.noreply.github.com>
2021-04-21 23:54:43 -0500

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,7 @@
 // https://medium.com/touch4it/end-to-end-testing-with-puppeteer-and-jest-ec8198145321
 module.exports = {
     launch: {
-        headless: true,
+        headless: false,
         slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
         devtools: true,
         timeout: 3000000, //  5 minute timeout
